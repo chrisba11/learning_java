c7bb1d04f508cd7857a4962709129aa40dc05608
Chris Ball <chrisball7@gmail.com>
2021-02-06 14:07:20 -0800

    learning about how short + short = int

--- PrintTests.java
+++ PrintTests.java
@@ -2,10 +2,12 @@ public class PrintTests
 {
   public static void main(String[] args)
   {
-    double result;
-    
-    result = 3 * Math.pow(6.0, 3.0);
-    System.out.println(result);
+    short num1, num2, num3;
+
+    num1 = 10;
+    num2 = 20;
+    num3 = (short)(num1 + num2);
+    System.out.println(num3);
 
 
   }
