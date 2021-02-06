c9266dc0d769421ec613e85ef2fa5ad3c91b842e
Chris Ball <chrisball7@gmail.com>
2021-02-06 13:25:34 -0800

    but if integer division is used and stored in a float type variable, it is stored as a float

--- PrintTests.java
+++ PrintTests.java
@@ -3,8 +3,13 @@ public class PrintTests
   public static void main(String[] args)
   {
     int num1 = 12;
+    float floatNum;
     System.out.println(num1 * 0.25);
     System.out.println(num1 / 4);
     System.out.println(num1 / 5);
+
+    floatNum = num1 / 5;
+
+    System.out.println(floatNum);
   }
 }
