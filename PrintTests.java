d58a54725764463ac02bf552c7a1892ba1b2fab9
Chris Ball <chrisball7@gmail.com>
2021-02-06 13:42:15 -0800

    testing out example using Math.pow in the book that was incorrect

--- PrintTests.java
+++ PrintTests.java
@@ -2,14 +2,11 @@ public class PrintTests
 {
   public static void main(String[] args)
   {
-    int num1 = 12;
-    float floatNum;
-    System.out.println(num1 * 0.25);
-    System.out.println(num1 / 4);
-    System.out.println(num1 / 5);
+    double result;
+    
+    result = 3 * Math.pow(6.0, 3.0);
+    System.out.println(result);
 
-    floatNum = num1 / 5;
 
-    System.out.println(floatNum);
   }
 }
